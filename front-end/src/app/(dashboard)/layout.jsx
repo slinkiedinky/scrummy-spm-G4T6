0e866f7010967d130eee8766e82bcbde58093b45
melvinlec <melvin.leck.2023@scis.smu.edu.sg>
2025-09-28 18:43:57 +0800

    Revert "Merge pull request #3 from slinkiedinky/SCRUM-19-generate-project-report-layout"
    This reverts commit 9103d50133f4f21ee430340cd036a84fd2cc2b3b, reversing
    changes made to 51b3af72fbbc762ec3d6229ebca42e29559265c9.

---
+++ front-end/src/app/(dashboard)/layout.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import AuthGuard from "@/components/AuthGuard";
+import { Sidebar } from "@/components/Sidebar";
+
+export default function DashboardLayout({ children }) {
+  return (
+    <AuthGuard>
+      <div className="flex h-screen bg-background">
+        <Sidebar />
+        <main className="flex-1 overflow-hidden">{children}</main>
+      </div>
+    </AuthGuard>
+  );
+}
